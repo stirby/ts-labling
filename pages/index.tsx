458af0c0ed374157bb34e07f64707613212be578
Stephen Kirby <me@skirby.dev>
2021-03-08 09:45:17 -0600

    changed labels for MDB

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { MongoClient } from "mongodb";
 import * as BoxSDK from "box-node-sdk";
 
 import { prependOnceListener } from "node:process";
+import { type } from "os";
 
 interface Props {
   imageID: string;
@@ -14,7 +15,23 @@ interface Props {
 }
 
 interface CompleteRequest {
+  carLabel: string;
   imageID: string;
+}
+
+// Label range reference
+const labelSet = {
+  
+  // Applied to north and south selection. Only one may be selected.
+  congestion: ["congested", "non-congested", "unclear"],
+
+  // Precipitation artifact, fog can be general only one may be selected. 
+  precipitation: ["rain", "snow", "fog", "clear"],
+
+  // Position artifact refers to photos where the camera aimed toward the highway.
+  // Lens referring to glare, obstruction
+  // environment referring to some physical anomaly to be inspected. 
+  artifact: ["position", "lens", "environment", "no-artifact"]
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
@@ -44,6 +61,17 @@ export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
 
   // Labels submitted by user
   if (ctx.req.method === "POST") {
+    // We return empty props here just to fufill the func...
+    // The request ends above.
+    const emptyResponse = {
+      props: {
+        imageID: "",
+        imageName: "",
+        imageContent: "",
+      },
+    };
+
+    // Parse request
     const data = await new Promise<string>((resolve, reject) => {
       const d: Uint8Array[] = [];
       ctx.req.on("data", (c) => d.push(c));
@@ -52,21 +80,29 @@ export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
     });
     const msg: CompleteRequest = JSON.parse(data);
 
-    console.log(msg)
+    // Check for incomplete labels
+    if (msg.carLabel == "") {
+      ctx.res.writeHead(500);
+      ctx.res.write(
+        "Missing Labels: 'carLabel' - Please complete before submitting."
+      );
+      ctx.res.end();
+      return emptyResponse;
+    }
+
+    // Update Sample in DB
+    const query = await collection.findOneAndUpdate(
+      { box_id: msg.imageID },
+      {
+        $set: { review: "complete", "labels.has_cars": msg.carLabel == "true" },
+      },
+      { upsert: false }
+    );
 
     ctx.res.writeHead(200);
-    ctx.res.write("success!");
+    ctx.res.write("Successful Submission, Thank you :)");
     ctx.res.end();
-
-    // We return empty props here just to fufill the func...
-    // The request ends above.
-    return {
-      props: {
-        imageID: "",
-        imageName: "",
-        imageContent: "",
-      },
-    };
+    return emptyResponse;
   }
 
   // Set up box folder
@@ -103,48 +139,62 @@ export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
     stream.on("end", () => resolve(Buffer.concat(d)));
     stream.on("error", () => reject());
   });
-
   imgProps.imageContent += data.toString("base64");
-  
 
   // Close mongoDB
-  client.close()
-
-  return {
-    props: imgProps,
-  };
+  client.close();
+  return { props: imgProps };
 };
 
 // Page Delcaration
 const Index: React.FC<Props> = (props) => {
   // Holds state as to whether the app is submitting a request or not.
   const [submitting, setSubmitting] = React.useState(false);
+
   // Holds state as to whether the app recieved an error from an API request.
   const [error, setError] = React.useState("");
 
-  // Holds state for the sample's "company" label
-  const [companyLabel, setCompanyLabel] = React.useState("");
+  // Holds state as to whether the client is currently submitting a sample to the server
+  const [status, setStatus] = React.useState("");
 
-  console.log("- Props.ImageContent:", props.imageContent);
+
+  //* Label States
+  // Holds state for the sample's "has_Cars" label
+  const [precipValue, setPrecip] = React.useState(""),
+        [artifactValue, setArtifact] = React.useState(""),
+        [congestionValue, setCongestion] = React.useState("");
 
   // Submits to `/`... aka getServerSideProps then
   // routes inside the POST block.
   const submit = () => {
     setSubmitting(true);
+
+    if (carLabel == "") {
+      console.log("Submission falied, incomplete labels.");
+    }
+
+    console.log("Submitting Value:", carLabel, "isNull:", carLabel == "");
+
     const request: CompleteRequest = {
-      imageID: "",
+      carLabel: carLabel,
+      imageID: props.imageID,
     };
 
     fetch("/", {
       body: JSON.stringify(request),
       method: "POST",
     })
-      .then(() => {
-        // We reload since we get new data on each load.
-        window.location.reload();
-        setTimeout(() => {
+      .then(async (response) => {
+        if (response.status === 500) {
+          setError(await response.text());
           setSubmitting(false);
-        }, 5000);
+          return;
+        } else if (response.status === 200) {
+          // Success Message Display
+          setError("");
+          setStatus(await response.text())
+        }
+        window.location.reload();
       })
       .catch((ex) => {
         setError(ex.toString());
@@ -152,44 +202,34 @@ const Index: React.FC<Props> = (props) => {
       });
   };
 
+  const newSample = () =>{
+    window.location.reload()
+  }
+
   return (
     <>
       <Head>
         <meta name="theme-color" content="rgb(0, 0, 0)" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
+      </Head>
 
-
-        <style global jsx>{`
-          body {
-            font-family: 'Cabin', sans-serif;
-          }
-        `}</style>
-      
+      <style global jsx>{`
+        body {
+          font-family: "Cabin", sans-serif;
+        }
+      `}</style>
 
       <body>
-
         <div className="headerGrid">
           <text className="headText">TrafficNet - Labeling Interface</text>
-          {error}
         </div>
 
         <div className="sampleGrid">
-          
-         
-          
           <div className="sampleTray">
-            
-            <img 
-              src={props.imageContent}
-              alt={props.imageID}
-            />
+            <img src={props.imageContent} alt={props.imageID} />
           </div>
-          
-
 
           <div className="labels">
-
             <text className="labelHead">Active Sample</text>
             <ul>
               <li className="sampleData">ID: {props.imageID}</li>
@@ -197,29 +237,52 @@ const Index: React.FC<Props> = (props) => {
             </ul>
 
             <br></br>
-            
+
             <text className="labelHead">Label Fields</text>
             <Radios
-              title="Company"
-              options={["audi", "volvo", "ford"]}
+              title="Congestion - North"
+              options={["true", "false"]}
               onChange={(value) => {
-                setCompanyLabel(value);
+                setCarLabel(value);
               }}
             ></Radios>
 
-            
-            <button className="submitChoice" disabled={submitting} onClick={submit}>
+            <Radios
+              title="Congestion - South"
+              options={["Congested", "Not Congested",]}
+              onChange={(value) => {
+                setCarLabel(value);
+              }}
+            ></Radios>
+
+            <Radios
+              title="Precipitation"
+              options={["clear", "rain", "snow", "fog"]}
+              onChange={(value) => {
+                SetPrecipLabel(value);
+              }}
+            ></Radios>
+
+
+            <button
+              className="submitChoice"
+              disabled={submitting}
+              onClick={submit}
+            >
               Submit
             </button>
-            <button className="skipChoice" disabled={false}>
+            <button className="skipChoice" disabled={submitting} onClick={newSample}>
               New Sample
             </button>
-            
-          
+
+
           </div>
+          <text className="err">{error}</text>
+          <text className="status">{status}</text>
+        </div>
         
 
-        </div>
+        
       </body>
     </>
   );
@@ -227,32 +290,37 @@ const Index: React.FC<Props> = (props) => {
 
 export default Index;
 
+/* React Functional Component
+* Creates a renderable custom react component
+*
+*/
 const Radios: React.FC<{
   title: string;
   options: string[];
   onChange: (value: string) => void;
 }> = (props) => {
-  const [selectedValue, setSelectedValue] = React.useState(props.options[0]);
+  const [selectedValue, setSelectedValue] = React.useState("");
 
   return (
-    <div>
-      <h3>{props.title}</h3>
+    <div className="radioBox">
+      <text className="radioTitle">{props.title}</text>
       {props.options.map((value: string) => {
         return (
-          //
           <React.Fragment key={value}>
-            <input
-              type="radio"
-              name={props.title}
-              value={value}
-              checked={value == selectedValue}
-              onChange={(event) => {
-                console.log(value);
-                setSelectedValue(event.target.value);
-                props.onChange(event.target.value);
-              }}
-            />
-            <label>{value}</label>
+            <div className="radioOpt">
+              <input
+                type="radio"
+                name={props.title}
+                value={value}
+                checked={value == selectedValue}
+                onChange={(event) => {
+                  console.log("Updated Label '" + props.title + "' to:", value);
+                  setSelectedValue(event.target.value);
+                  props.onChange(event.target.value);
+                }}
+              />
+              <label>{value}</label>
+            </div>
           </React.Fragment>
         );
       })}
